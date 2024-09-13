bfa7cdaab89e8fa1f115dd5bc039451c01bc9a7b
Oniicyan <130678414+Oniicyan@users.noreply.github.com>
2024-09-13 10:47:50 +0800

    Create BTNScriptBC_TASK.ps1

---
+++ BTNScriptBC_TASK.ps1
@@ -0,0 +1,24 @@
+if (!(Test-Path $USERPATH\STARTUP.cmd)) {
+	Read-Host `n未配置 BTNScriptBC，按 Enter 键结束...
+	return
+}
+if ((Fltmc).Count -eq 3) {
+	if (Test-Path $APPWTPATH) {
+		$PROCESS = "$APPWTPATH -ArgumentList `"powershell $($MyInvocation.MyCommand.Definition)`""
+	} else {
+		$PROCESS = "powershell -ArgumentList `"$($MyInvocation.MyCommand.Definition)`""
+	}
+	Write-Host "`n  10 秒后以管理员权限继续执行"
+	timeout 10
+	Invoke-Expression "Start-Process $PROCESS -Verb RunAs"
+	return
+}
+$USERPATH = "$ENV:USERPROFILE\BTNScriptBC"
+$PRINCIPAL = New-ScheduledTaskPrincipal -UserId $ENV:COMPUTERNAME\$ENV:USERNAME -RunLevel Highest
+$SETTINGS = New-ScheduledTaskSettingsSet -RunOnlyIfNetworkAvailable -RestartCount 5 -RestartInterval (New-TimeSpan -Seconds 60) -AllowStartIfOnBatteries
+$TRIGGER = New-ScheduledTaskTrigger -AtLogon -User $ENV:COMPUTERNAME\$ENV:USERNAME
+$ACTION = New-ScheduledTaskAction -Execute "$USERPATH\STARTUP.cmd"
+$TASK = New-ScheduledTask -Principal $PRINCIPAL -Settings $SETTINGS -Trigger $TRIGGER -Action $ACTION
+Unregister-ScheduledTask BTNScriptBC_STARTUP -Confirm:$false -ErrorAction Ignore
+Register-ScheduledTask BTNScriptBC_STARTUP -InputObject $TASK | Out-Null
+Write-Host "`n  已配置自启动任务计划：BTNScriptBC_STARTUP`n"
