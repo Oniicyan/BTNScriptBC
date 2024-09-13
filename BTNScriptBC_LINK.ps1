a59a72615d6b95a04a3fdbd6abf321fc06815e87
Oniicyan <130678414+Oniicyan@users.noreply.github.com>
2024-09-13 09:28:31 +0800

    Update BTNScriptBC_LINK.ps1

--- BTNScriptBC_LINK.ps1
+++ BTNScriptBC_LINK.ps1
@@ -1,11 +1,12 @@
 $USERPATH = "$ENV:USERPROFILE\BTNScriptBC"
-$LINKPATH = "$([Environment]::GetFolderPath("Desktop"))\BTNScriptBC.lnk"
+$LINKPATH = "$USERPATH\BTNScriptBC.lnk"
 $WshShell = New-Object -COMObject WScript.Shell
-$Shortcut = $WshShell.CreateShortcut("$([Environment]::GetFolderPath("Desktop"))\BTNScriptBC.lnk")
+$Shortcut = $WshShell.CreateShortcut($LINKPATH)
 $Shortcut.TargetPath = "$USERPATH\STARTUP.cmd"
 $Shortcut.IconLocation = "$ENV:WINDIR\System32\EaseOfAccessDialog.exe"
 $Shortcut.Save()
-$LINKBYTE = [System.IO.File]::ReadAllBytes($Shortcut.FullName)
+$LINKBYTE = [System.IO.File]::ReadAllBytes($LINKPATH)
 $LINKBYTE[0x15] = $LINKBYTE[0x15] -bor 0x20
-[System.IO.File]::WriteAllBytes($Shortcut.FullName,$LINKBYTE)
+[System.IO.File]::WriteAllBytes($LINKPATH,$LINKBYTE)
+Copy-Item $LINKPATH $([Environment]::GetFolderPath("Desktop"))
 Write-Host "`n  已配置桌面快捷方式：BTNScriptBC.lnk`n"
