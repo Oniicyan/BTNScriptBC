21329265b7fa74de83194bec800f5388c2aea1ec
Oniicyan <130678414+Oniicyan@users.noreply.github.com>
2024-09-14 17:41:37 +0800

    Update BTNScriptBC_LINK.ps1

--- BTNScriptBC_LINK.ps1
+++ BTNScriptBC_LINK.ps1
@@ -1,13 +1,17 @@
 $USERPATH = "$ENV:USERPROFILE\BTNScriptBC"
 if (!(Test-Path $USERPATH\STARTUP.cmd)) {Write-Host `n  未配置 BTNScriptBC`n; return}
+if ((Get-Content $USERPATH\STARTUP.cmd) -Match 'nofw') {$NOFW = 1}
 $LINKPATH = "$USERPATH\BTNScriptBC.lnk"
+if ($NOFW) {$LINKPATH = "$USERPATH\BTNScriptBC_nofw.lnk"}
 $WshShell = New-Object -COMObject WScript.Shell
 $Shortcut = $WshShell.CreateShortcut($LINKPATH)
 $Shortcut.TargetPath = "$USERPATH\STARTUP.cmd"
 $Shortcut.IconLocation = "$ENV:WINDIR\System32\EaseOfAccessDialog.exe"
 $Shortcut.Save()
-$LINKBYTE = [System.IO.File]::ReadAllBytes($LINKPATH)
-$LINKBYTE[0x15] = $LINKBYTE[0x15] -bor 0x20
-[System.IO.File]::WriteAllBytes($LINKPATH,$LINKBYTE)
+if (!$NOFW) {
+	$LINKBYTE = [System.IO.File]::ReadAllBytes($LINKPATH)
+	$LINKBYTE[0x15] = $LINKBYTE[0x15] -bor 0x20
+	[System.IO.File]::WriteAllBytes($LINKPATH,$LINKBYTE)
+}
 Copy-Item $LINKPATH $([Environment]::GetFolderPath("Desktop"))
-Write-Host "`n  已配置桌面快捷方式：BTNScriptBC.lnk`n"
+Write-Host "`n  已配置桌面快捷方式：$($LINKPATH.Split('\')[-1])`n"
